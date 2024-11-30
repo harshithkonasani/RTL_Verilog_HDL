675044ca7f61e7f5f0b0549229f9f86ecd324962
KONASANI HARSHITH <94668868+harshithkonasani@users.noreply.github.com>
2024-11-30 14:17:37 +0530

    single_port_memory.v

--- Memory/single_port_memory.v
+++ Memory/single_port_memory.v
@@ -21,3 +21,41 @@ begin
 end
        assign data_io  = (cs && ~write_en && read_en)?temp:8'bzzzz_zzzz;
 endmodule
+
+
+
+
+//TESTBENCH FOR ABOVE MEMORY 
+
+module tb_memory_io();
+parameter data_size = 8, address = 4;
+reg clk;
+reg [address-1:0] address_in;
+reg write_en, read_en,cs;
+wire [data_size-1:0] data_io;
+
+memory_inout #(.data_size(8), .address(4)) DUT (
+.clk(clk), .address_in(address_in), .write_en(write_en),
+.read_en(read_en), .cs(cs), .data_io(data_io) );
+
+reg [data_size-1:0] temp;
+assign data_io = (cs && write_en && read_en)?temp:8'bzzzz_zzzz;
+always #5 clk = ~clk;
+always #10 address_in = address_in +1;
+always #160 write_en = ~write_en;
+always #400 cs = ~cs;
+always #160 read_en = ~read_en;
+//always #10 data_io = data_io + 1;
+always #10 temp = $random();  // used to generate randpm data
+
+initial 
+begin
+    temp = 0;
+    write_en = 1;
+    address_in =0;
+    read_en = 0;
+    clk = 0;
+    cs = 1;
+#500 $finish();
+end
+endmodule
