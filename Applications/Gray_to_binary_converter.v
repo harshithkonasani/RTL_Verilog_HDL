26c0d12fe3394bbca80f522323d553e1fa036426
KONASANI HARSHITH <94668868+harshithkonasani@users.noreply.github.com>
2024-11-30 12:05:30 +0530

    Gray_to_binary_converter.v

---
+++ Applications/Gray_to_binary_converter.v
@@ -0,0 +1,12 @@
+`timescale 1ns / 1ps
+module g2b_converter #(parameter WIDTH=4) 
+(input [WIDTH-1:0] gray, 
+output [WIDTH-1:0] binary
+);
+  genvar i;
+  generate
+    for(i=0;i<WIDTH;i=i+1) begin
+      assign binary[i] = ^(gray >> i);
+    end
+  endgenerate
+endmodule
